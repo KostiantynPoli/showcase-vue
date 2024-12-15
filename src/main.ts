e24a7e72d5f304ee8c603e9fdc61134c0258d263
Kostiantyn Polishchuk <kostiantyn@Kostiantyns-MacBook-Pro.local>
2024-12-15 23:16:05 +0100

    remove ref from main

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {createApp, ref} from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import {domainName, queryParams, urlParams, writeCookies} from "./api/offers.ts";
